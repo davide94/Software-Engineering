608426ab2ed697883ef7f36ba0335789883ac02c
Luca <luca.pirti@mail.polimi.it>
2016-05-04 13:30:12 +0200

    Implemented a part of NobilityCell

--- src/main/java/it/polimi/ingsw/cg26/model/board/NobilityCell.java
+++ src/main/java/it/polimi/ingsw/cg26/model/board/NobilityCell.java
@@ -1,22 +1,49 @@
 package it.polimi.ingsw.cg26.model.board;
 
+import java.util.HashSet;
+import java.util.Set;
+import it.polimi.ingsw.cg26.model.player.Player;
+import it.polimi.ingsw.cg26.model.bonus.*;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+
 /**
  * 
  */
 public class NobilityCell {
+	
+	
+	 /**
+     * The number of the Cell
+     */
+    private int CellNuber;
+
+    
+   /**
+    * The players on the nobility cell
+    */
+    private Set<Player> players= new HashSet<Player>();
+    
+    /**
+     * The bonus on the nobility cell
+     */
+    private List<Bonus> bonuses=new ArrayList<Bonus>();
+    
+    
+    
+    
 
     /**
      * Default constructor
      */
-    public NobilityCell() {
+    public NobilityCell(int CellNumber) {
+    	this.CellNuber= CellNumber;
     }
 
-    /**
-     * 
-     */
-    private int CellNuber;
-
-
+   
     /**
      * @return
      */
--- src/main/java/it/polimi/ingsw/cg26/model/board/NobilityTrack.java
+++ src/main/java/it/polimi/ingsw/cg26/model/board/NobilityTrack.java
@@ -4,6 +4,14 @@ package it.polimi.ingsw.cg26.model.board;
  * 
  */
 public class NobilityTrack {
+	
+	/**
+	 * 
+	 */
+	private NobilityCell[] cells;
+	
+	
+	
 
     /**
      * Default constructor
