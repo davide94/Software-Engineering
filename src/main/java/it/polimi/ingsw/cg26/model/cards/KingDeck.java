0fd7176b6ed2b0211aa99e638fb4f02f8777a93e
Luca <luca.pirti@mail.polimi.it>
2016-05-11 21:15:15 +0200

    Created King Reward Deck that exstend Deck class

---
+++ src/main/java/it/polimi/ingsw/cg26/model/cards/KingDeck.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.cg26.model.cards;
+
+import java.util.Collection;
+
+import it.polimi.ingsw.cg26.model.bonus.*;
+
+public class KingDeck extends Deck<VictoryBonus> {
+	
+	/**
+    *
+    */
+   public  KingDeck(Collection<VictoryBonus> c) {
+       super(c);
+   }
+	
+	
+
+}
