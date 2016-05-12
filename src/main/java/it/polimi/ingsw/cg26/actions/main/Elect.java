23dff8b5681913f56dd1388d43c3cea322c83298
MarcoRe <marco94.re@gmail.com>
2016-05-12 21:35:00 +0200

    Implemented elect action

--- src/main/java/it/polimi/ingsw/cg26/actions/main/Elect.java
+++ src/main/java/it/polimi/ingsw/cg26/actions/main/Elect.java
@@ -1,7 +1,12 @@
 package it.polimi.ingsw.cg26.actions.main;
 
 import it.polimi.ingsw.cg26.actions.Action;
+import it.polimi.ingsw.cg26.exceptions.NoRemainingActionsException;
+import it.polimi.ingsw.cg26.exceptions.NotExistingCouncillorException;
 import it.polimi.ingsw.cg26.model.board.GameBoard;
+import it.polimi.ingsw.cg26.model.cards.Councillor;
+import it.polimi.ingsw.cg26.model.cards.PoliticColor;
+import it.polimi.ingsw.cg26.model.player.Player;
 
 /**
  *
@@ -10,18 +15,45 @@ public class Elect extends Action {
 
     private final String region;
 
-    private final String assistantColor;
+    private final PoliticColor councillorColor;
 
-    public Elect(String region, String assistantColor) {
-        if (region == null || assistantColor == null)
+    public Elect(String region, PoliticColor councillorColor) {
+        if (region == null || councillorColor == null)
             throw new NullPointerException();
         this.region = region;
-        this.assistantColor = assistantColor;
+        this.councillorColor = councillorColor;
     }
 
+    /**
+     * 
+     */
     @Override
     public void apply(GameBoard gameBoard) {
-        // TODO
+        
+    	Player currentPlayer = gameBoard.getCurrentPlayer();
+    	
+    	if (!currentPlayer.canPerformMainAction())
+    		throw new NoRemainingActionsException();
+    	Councillor addCouncillor=null;
+    	for(Councillor iterCouncillor : gameBoard.getCouncillorsPool()){
+    		if(iterCouncillor.getColor().equals(this.councillorColor)){
+    			addCouncillor = iterCouncillor;
+    			break;
+    		}
+    	}
+    	if(addCouncillor == null){
+    		throw new NotExistingCouncillorException();
+    	}
+    	Councillor droppedCouncillor;
+    	if("king".equalsIgnoreCase(this.region)){
+    		droppedCouncillor = gameBoard.getKingBalcony().elect(addCouncillor);
+    	} else {
+    		droppedCouncillor = gameBoard.getRegion(this.region).getBalcony().elect(addCouncillor);
+    	}
+    	gameBoard.getCouncillorsPool().remove(addCouncillor);
+    	gameBoard.getCouncillorsPool().add(droppedCouncillor);
+    	currentPlayer.addCoins(4);
+    	currentPlayer.performMainAction();
     }
 
 }
