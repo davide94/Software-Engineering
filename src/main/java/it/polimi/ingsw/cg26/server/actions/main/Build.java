f14728b8a47be2e003801a41a2ab7e4e12c4f071
Luca <luca.pirti@mail.polimi.it>
2016-05-23 17:13:20 +0200

    Improved some features for ColorBonus and added some methods

--- src/main/java/it/polimi/ingsw/cg26/server/actions/main/Build.java
+++ src/main/java/it/polimi/ingsw/cg26/server/actions/main/Build.java
@@ -6,7 +6,9 @@ import it.polimi.ingsw.cg26.server.exceptions.InvalidCardsException;
 import it.polimi.ingsw.cg26.server.exceptions.NoRemainingActionsException;
 import it.polimi.ingsw.cg26.server.exceptions.NoRemainingAssistantsException;
 import it.polimi.ingsw.cg26.server.model.board.City;
+import it.polimi.ingsw.cg26.server.model.board.CityColor;
 import it.polimi.ingsw.cg26.server.model.board.GameBoard;
+import it.polimi.ingsw.cg26.server.model.board.Region;
 import it.polimi.ingsw.cg26.server.model.cards.BusinessPermissionTile;
 import it.polimi.ingsw.cg26.server.model.player.Player;
 
@@ -35,6 +37,33 @@ public class Build extends Action {
         if (currentPlayer.getAssistantsNumber() < empNumber)
             throw new NoRemainingAssistantsException();
         realCity.build(currentPlayer);
+        
+        
+        CityColor color=realCity.getColor();
+        if(gameBoard.checkColorBonuses(currentPlayer, color)){
+        	
+        	currentPlayer.addVictoryPoints(10); //come faccio a sapere quanti ne devo dare?
+            //prendo tessera del re
+        	
+        }
+        
+        
+        //si può fare lo stesso per la regione però
+        //City dovrebbe avere il reference a Region
+        
+        /*
+         if(realCity.getRegion().checkRegionBonuses(currentPlayer)){
+         //applico il bonus
+         //prendo tessera re
+          
+          }
+          */
+        
+        
+        
+        
+        
+        
         currentPlayer.takeAssistants(empNumber);
         currentPlayer.useBPT(tile);
         currentPlayer.performMainAction();
