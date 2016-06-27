16f09ae0bb09be2bdaf21901c23c7a1bcd96b69a
MarcoRe <marco94.re@gmail.com>
2016-06-27 10:58:07 +0200

    Finish to implement actions pane

--- src/main/java/it/polimi/ingsw/cg26/client/ui/ActionsPane.java
+++ src/main/java/it/polimi/ingsw/cg26/client/ui/ActionsPane.java
@@ -1,7 +1,9 @@
 package it.polimi.ingsw.cg26.client.ui;
 
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.input.InputEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
@@ -81,50 +83,107 @@ public class ActionsPane extends AnchorPane {
 		for(Button b : actions) {
 			b.addEventHandler(MouseEvent.MOUSE_EXITED, e -> b.setStyle("-fx-background-color: transparent"));
 		}
-		acquire.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> 
+
+		EventHandler<InputEvent> acquireHandler = (InputEvent event) ->
 			acquire.setStyle("-fx-background-image: url(" + getClass().getResource("/img/actionsButtons/acquireMousePassing.png") + ");" +
+				"-fx-background-position: center;" +
+				"-fx-background-size: 100% 100%;" + 
+				"-fx-background-color: transparent");
+		acquire.addEventHandler(MouseEvent.MOUSE_ENTERED, acquireHandler);
+		acquire.addEventHandler(MouseEvent.MOUSE_RELEASED, acquireHandler);
+		acquire.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> 
+		acquire.setStyle("-fx-background-image: url(" + getClass().getResource("/img/actionsButtons/acquireMousePressed.png") + ");" +
                 "-fx-background-position: center;" +
                 "-fx-background-size: 100% 100%;" + 
                 "-fx-background-color: transparent"));
 		
-		buildKing.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> 
+		EventHandler<InputEvent> buildKingHandler = (InputEvent event) ->
 			buildKing.setStyle("-fx-background-image: url(" + getClass().getResource("/img/actionsButtons/buildKingMousePassing.png") + ");" +
+				"-fx-background-position: center;" +
+				"-fx-background-size: 100% 100%;" + 
+				"-fx-background-color: transparent");
+		buildKing.addEventHandler(MouseEvent.MOUSE_ENTERED, buildKingHandler);
+		buildKing.addEventHandler(MouseEvent.MOUSE_RELEASED, buildKingHandler);
+		buildKing.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> 
+			buildKing.setStyle("-fx-background-image: url(" + getClass().getResource("/img/actionsButtons/buildKingMousePressed.png") + ");" +
 				"-fx-background-position: center;" +
 				"-fx-background-size: 100% 100%;" + 
 	            "-fx-background-color: transparent"));
 		
-		electAsMainAction.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> 
+		EventHandler<InputEvent> electAsMainActionHandler = (InputEvent event) ->
 			electAsMainAction.setStyle("-fx-background-image: url(" + getClass().getResource("/img/actionsButtons/electAsMainActionMousePassing.png") + ");" +
 				"-fx-background-position: center;" +
 				"-fx-background-size: 100% 100%;" + 
+				"-fx-background-color: transparent");
+		electAsMainAction.addEventHandler(MouseEvent.MOUSE_ENTERED, electAsMainActionHandler);
+		electAsMainAction.addEventHandler(MouseEvent.MOUSE_RELEASED, electAsMainActionHandler);
+		electAsMainAction.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> 
+			electAsMainAction.setStyle("-fx-background-image: url(" + getClass().getResource("/img/actionsButtons/electAsMainActionMousePressed.png") + ");" +
+				"-fx-background-position: center;" +
+				"-fx-background-size: 100% 100%;" + 
                 "-fx-background-color: transparent"));
 		
-		build.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> 
+		EventHandler<InputEvent> buildHandler = (InputEvent event) ->
 			build.setStyle("-fx-background-image: url(" + getClass().getResource("/img/actionsButtons/buildMousePassing.png") + ");" +
 				"-fx-background-position: center;" +
 				"-fx-background-size: 100% 100%;" + 
+				"-fx-background-color: transparent");
+		build.addEventHandler(MouseEvent.MOUSE_ENTERED, buildHandler);
+		build.addEventHandler(MouseEvent.MOUSE_RELEASED, buildHandler);
+		build.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> 
+			build.setStyle("-fx-background-image: url(" + getClass().getResource("/img/actionsButtons/buildMousePressed.png") + ");" +
+				"-fx-background-position: center;" +
+				"-fx-background-size: 100% 100%;" + 
                 "-fx-background-color: transparent"));
 		
-		engageAssistant.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> 
+		EventHandler<InputEvent> engageAssistantHandler = (InputEvent event) ->
 			engageAssistant.setStyle("-fx-background-image: url(" + getClass().getResource("/img/actionsButtons/engageAssistantMousePassing.png") + ");" +
 				"-fx-background-position: center;" +
 				"-fx-background-size: 100% 100%;" + 
+				"-fx-background-color: transparent");
+		engageAssistant.addEventHandler(MouseEvent.MOUSE_ENTERED, engageAssistantHandler);
+		engageAssistant.addEventHandler(MouseEvent.MOUSE_RELEASED, engageAssistantHandler);
+		engageAssistant.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> 
+			engageAssistant.setStyle("-fx-background-image: url(" + getClass().getResource("/img/actionsButtons/engageAssistantMousePressed.png") + ");" +
+				"-fx-background-position: center;" +
+				"-fx-background-size: 100% 100%;" + 
 				"-fx-background-color: transparent"));
 		
-		changeBPT.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> 
+		EventHandler<InputEvent> changeBPTHandler = (InputEvent event) ->
 			changeBPT.setStyle("-fx-background-image: url(" + getClass().getResource("/img/actionsButtons/changeBPTMousePassing.png") + ");" +
 				"-fx-background-position: center;" +
 				"-fx-background-size: 100% 100%;" + 
-				"-fx-background-color: transparent"));
-		
-		electAsQuickAction.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> 
-			electAsQuickAction.setStyle("-fx-background-image: url(" + getClass().getResource("/img/actionsButtons/electAsQuickActionMousePassing.png") + ");" +
+				"-fx-background-color: transparent");
+		changeBPT.addEventHandler(MouseEvent.MOUSE_ENTERED, changeBPTHandler);
+		changeBPT.addEventHandler(MouseEvent.MOUSE_RELEASED, changeBPTHandler);
+		changeBPT.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> 
+			changeBPT.setStyle("-fx-background-image: url(" + getClass().getResource("/img/actionsButtons/changeBPTMousePressed.png") + ");" +
 				"-fx-background-position: center;" +
 				"-fx-background-size: 100% 100%;" + 
 				"-fx-background-color: transparent"));
 		
-		additionalMainAction.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> 
+		EventHandler<InputEvent> electAsQuickActionHandler = (InputEvent event) ->
+			electAsQuickAction.setStyle("-fx-background-image: url(" + getClass().getResource("/img/actionsButtons/electAsQuickActionMousePassing.png") + ");" +
+				"-fx-background-position: center;" +
+				"-fx-background-size: 100% 100%;" + 
+				"-fx-background-color: transparent");
+		electAsQuickAction.addEventHandler(MouseEvent.MOUSE_ENTERED, electAsQuickActionHandler);
+		electAsQuickAction.addEventHandler(MouseEvent.MOUSE_RELEASED, electAsQuickActionHandler);
+		electAsQuickAction.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> 
+			electAsQuickAction.setStyle("-fx-background-image: url(" + getClass().getResource("/img/actionsButtons/electAsQuickActionMousePressed.png") + ");" +
+				"-fx-background-position: center;" +
+				"-fx-background-size: 100% 100%;" + 
+				"-fx-background-color: transparent"));
+		
+		EventHandler<InputEvent> additionalMainActionHandler = (InputEvent event) ->
 			additionalMainAction.setStyle("-fx-background-image: url(" + getClass().getResource("/img/actionsButtons/additionalMainActionMousePassing.png") + ");" +
+				"-fx-background-position: center;" +
+				"-fx-background-size: 100% 100%;" + 
+				"-fx-background-color: transparent");
+			additionalMainAction.addEventHandler(MouseEvent.MOUSE_ENTERED, additionalMainActionHandler);
+		additionalMainAction.addEventHandler(MouseEvent.MOUSE_RELEASED, additionalMainActionHandler);
+		additionalMainAction.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> 
+			additionalMainAction.setStyle("-fx-background-image: url(" + getClass().getResource("/img/actionsButtons/additionalMainActionMousePressed.png") + ");" +
 				"-fx-background-position: center;" +
 				"-fx-background-size: 100% 100%;" + 
 				"-fx-background-color: transparent"));
