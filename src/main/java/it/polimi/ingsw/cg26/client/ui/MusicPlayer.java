1bf9e62a50a6a68f8eb2f6c08589422eac26825b
Luca <luca.pirti@mail.polimi.it>
2016-07-03 21:10:00 +0200

    Implemented Music Player for the sound and musicPane for the GUI

---
+++ src/main/java/it/polimi/ingsw/cg26/client/ui/MusicPlayer.java
@@ -0,0 +1,64 @@
+package it.polimi.ingsw.cg26.client.ui;
+
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+import javafx.embed.swing.JFXPanel;
+
+public class MusicPlayer extends JFXPanel{
+	
+	private final String path= "src/main/resources/sound/gigi.wav";
+	private File file;
+	
+	private AudioInputStream audioInputStream;
+	
+	private Clip clip;
+	
+	
+	public MusicPlayer(){
+		
+		
+		try{
+			
+			file=new File(path);
+			audioInputStream= AudioSystem.getAudioInputStream(file.getAbsoluteFile());
+			
+			clip= AudioSystem.getClip();
+			clip.open(audioInputStream);
+		}catch (Exception e){
+			System.out.println(e.getMessage());
+		}
+		
+		play();
+	}
+	
+	public void play(){
+		
+		try{
+		    clip.start();
+			clip.loop(clip.LOOP_CONTINUOUSLY);
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	
+	public void stop(){
+		
+		try{
+			
+			clip.stop();
+		}catch (Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public boolean isRunning(){
+		return clip.isRunning();
+	}	
+ 
+
+}
